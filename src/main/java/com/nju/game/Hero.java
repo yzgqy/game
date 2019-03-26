be84ad31c5e37dca266ef1deeb331142882a7191
yzgqy <772751879@qq.com>
2019-03-26 15:57:12 +0800

    注释

--- src/main/java/com/nju/game/Hero.java
+++ src/main/java/com/nju/game/Hero.java
@@ -9,13 +9,13 @@ import lombok.ToString;
 @ToString
 public class Hero {
 //    攻击力、护甲值、当前生命值、总生命值、等级、当前经验值、等级经验值、（法力值）
-    private int attackPoint;
-    private int armorPoint;
-    private int currentHealthPoint;
-    private int totalHealthPoint;
-    private int level;
-    private int currentXP;
-    private int levelXP;
-    private int currentMana;
-    private int levelMana;
+    private int attackPoint; //攻击力
+    private int armorPoint;//护甲值
+    private int currentHealthPoint;//当前生命值
+    private int totalHealthPoint;//总生命值
+    private int level;//等级
+    private int currentXP;//当前经验值
+    private int levelXP;//等级经验值
+    private int currentMana;//当前法力值
+    private int levelMana;//等级法力值
 }
